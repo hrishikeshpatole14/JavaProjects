8b84b1edd1b584e0cf6f82fdee9b451d27ef352e
hrishikeshpatole14 <hrishikeshpatole1@gmail.com>
2025-06-23 21:35:33 +0530

    added java file

---
+++ Expression.java
@@ -0,0 +1,14 @@
+// find the result of the following expression
+// float a = 7/4 * 9/2
+
+
+
+
+public class Expression {
+    public static void main(String[] args) {
+        
+    float a = 7/4.0f * 9/2.0f;
+    System.out.println(a);
+
+    }
+}
