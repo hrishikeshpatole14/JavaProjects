ccb2f24f6ef2924d618e3e59d42dab5acda7d40f
hrishikeshpatole14 <hrishikeshpatole1@gmail.com>
2025-06-23 21:44:57 +0530

    added

---
+++ Operators.java
@@ -0,0 +1,36 @@
+
+
+public class Operators {
+    public static void main(String[] args) {
+        // 1. Arithmetic Operators
+        int a = 4;
+        // int b = 6 % a // Modulo operator
+        int b = 6 + a;
+
+        // 2. Assignment operators
+        int c = 9;
+        c *= 3;
+       System.out.println(c);
+
+       // 3. comparison operators
+       System.out.println(64<6);
+
+       // 4. Logical operators
+       System.out.println(64>5 && 64>56);
+       System.out.println(64>5 || 64>98);
+
+       // 5. bitwiase operators
+       System.out.println(2&3);
+
+
+        
+        
+
+
+
+
+
+
+
+    }
+}
