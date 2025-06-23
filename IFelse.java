8aba27716b50179544132e4529291bd55b248b74
hrishikeshpatole14 <hrishikeshpatole1@gmail.com>
2025-06-23 21:39:58 +0530

    added java file

---
+++ IFelse.java
@@ -0,0 +1,39 @@
+
+
+
+public class IFelse {
+    public static void main(String[] args) {
+        
+     /*int age = 15;
+     if(age>18){
+        System.out.println("Yes boy u can drive!");
+
+     }
+
+     else{
+        System.out.println("No boy u cannot drive!");
+     }
+     */
+
+
+      int age = 10;
+     if(age==18 && age>=18){
+        System.out.println("Yes boy u can drive!");
+
+     }
+     else{
+        System.out.println("No boy u cannot drive!");
+     }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
