9c382742cd39a0a7a2c0ec0eef20c5831796412a
hrishikeshpatole14 <hrishikeshpatole1@gmail.com>
2025-06-23 21:44:03 +0530

    added

---
+++ Literals.java
@@ -0,0 +1,19 @@
+
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        boolean a = true;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(a);
+
+
+
+
+    }
+}
