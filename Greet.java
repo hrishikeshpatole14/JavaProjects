24bb0b34b9bef1c75ebd059197cf2b00b4186bf9
hrishikeshpatole14 <hrishikeshpatole1@gmail.com>
2025-06-23 21:37:56 +0530

    added java file

---
+++ Greet.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+// ask user his name and greet with hello<name> have a god day
+
+
+
+public class Greet {
+
+    public static void main(String[] args){
+
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Enter your name:");
+ String name = sc.next();
+
+ System.out.println(" Hello " + name+ " have a good day!");
+
+
+    }
+
+
+}
