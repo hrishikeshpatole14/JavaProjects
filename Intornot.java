b524e04a3ec0ed9103bdd6f3e0a53c4a2b11fbd5
hrishikeshpatole14 <hrishikeshpatole1@gmail.com>
2025-06-23 21:41:53 +0530

    added

---
+++ Intornot.java
@@ -0,0 +1,16 @@
+// checking whether the num is int or not.
+
+import java.util.Scanner;
+
+public class Intornot {
+    
+public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your number:");
+    System.out.println(sc.hasNextInt());
+    // sc.hasNextInt is used to check num is int or not
+    // give anser is true or false
+    
+}
+}
